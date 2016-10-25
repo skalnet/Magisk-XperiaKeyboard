f2024c9768977232c4b3deaa6f68e2860f7e38b6
skalnet <eike@skal-net.de>
2016-10-25 17:00:43 +0200

    initial commit

--- config.sh
+++ config.sh
@@ -36,7 +36,7 @@ CACHEMOD=false
 
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=XperiaKeyboard
 
 # Set to true if you need automount
 # Most mods would like it to be enabled
@@ -59,7 +59,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "  Magisk XperiaKeyboard Module "
   ui_print "*******************************"
 }
 
